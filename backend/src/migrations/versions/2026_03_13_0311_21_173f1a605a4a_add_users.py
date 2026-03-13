c6ee1295f3e2da6b689c2b8a82925753f002f462
Roman Abazatov <raabazatov@mai.education.ru>
2026-03-13 17:03:15 +0300

    feat(auth): add database connection and initial user migration

---
+++ backend/src/migrations/versions/2026_03_13_0311_21_173f1a605a4a_add_users.py
@@ -0,0 +1,25 @@
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+revision: str = "X173f1a605a4a"
+down_revision: Union[str, Sequence[str], None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.create_table(
+        "users",
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column("email", sa.String(length=200), nullable=False),
+        sa.Column("hashed_password", sa.String(length=200), nullable=False),
+        sa.Column("is_active", sa.Boolean(), nullable=False),
+        sa.PrimaryKeyConstraint("id"),
+        sa.UniqueConstraint("email"),
+    )
+
+
+def downgrade() -> None:
+    op.drop_table("users")
