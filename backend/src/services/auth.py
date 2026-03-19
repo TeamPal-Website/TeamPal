31139ebe224ac93e732bb25b1f617ac55d89a5e3
Roman Abazatov <raabazatov@mai.education.ru>
2026-03-19 21:26:46 +0300

    fix(deps): fix requirements.txt after merge

--- backend/src/services/auth.py
+++ backend/src/services/auth.py
@@ -15,7 +15,7 @@ class AuthService:
         if expires_delta:
             expire = datetime.now(timezone.utc) + expires_delta
         else:
-            expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+            expire = datetime.now(timezone.utc) + timedelta(minutes=settings.JWT_ACCESS_TOKEN_EXPIRE_MINUTES)
         to_encode.update({"exp": expire})
         encoded_jwt = jwt.encode(to_encode, settings.JWT_SECRET_KEY, algorithm=settings.JWT_ALGORITHM)
         return encoded_jwt
